c81ba0f24dfde1c88161acdd2504c4072d9d9d44
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-08-12 15:25:28 +0530

    Merge branch 'main'

---
+++ front-end/src/Containers/homePage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '../Styles/Home.css';
+
+class HomePage extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
