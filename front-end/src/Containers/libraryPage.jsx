9d19853111cb95fbfe698c21c5f8ba4f57ac3b1f
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-08-12 15:25:28 +0530

    Merge branch 'main'

---
+++ front-end/src/Containers/libraryPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import '../Styles/Library.css';
+
+class LibraryPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="Library">
+        <h1>Library</h1>
+      </div>
+    );
+  }
+}
+
+export default LibraryPage;
