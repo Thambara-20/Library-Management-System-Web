3298d52e4a2658aef3d37ba4ed92f9b9e4574461
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-08-19 22:29:27 +0530

    Initiate Sign Up Page

---
+++ front-end/src/Containers/CustomerSupport/CustomerSupport.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../../Styles/CustomerSupport.css"
+import { useRef, useState } from 'react'
+import ContactDetails from '../../Components/ContactDetails';
+// import emailjs from '@emailjs/browser';
+
+
+const CustomerSupport = () => {
+    
+    return (
+        <div>   
+        <div></div>
+        <ContactDetails/>
+        </div>
+        
+    );
+};
+
+export default CustomerSupport;
