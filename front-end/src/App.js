e923c6acfb18caf7b10422f84ecf4bcf59542503
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-09-03 23:57:44 +0530

    Initiate Add, Update and Remove Book Pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import MainFooter from './Components/Footer';
-// import Home from '../src/Containers/Home/HomePage';
-import Home from './Containers/Home/Home';
+import Home from '../src/Containers/Home/HomePage';
 import { Component } from 'react';
 import SignInPage from './Containers/SignInPage/SignInPage';
 import LibraryPage from './Containers/Library/LibraryPage';
@@ -12,6 +11,9 @@ import AdminMainPage from './Containers/Admin/AdminMainPage';
 import UserManagement from './Containers/Admin/UserManagement';
 import BookManagement from './Containers/Admin/BookManagement';
 
+import BookAdd from './Containers/Admin/AddBook';
+import BookRemove from './Containers/Admin/RemoveBook';
+import BookUpdate from './Containers/Admin/UpdateBook';
 
 
 class App extends Component{
@@ -23,13 +25,21 @@ class App extends Component{
           <Route path="/Library" element={<LibraryPage/>} />
           <Route path="/ContactUs"  element={<CustomerSuppportPage/>}/>
           <Route path="/"   element ={<Home/>} />
-          {/* <Route path="/SignInPage"   element ={<SignInPage/>} /> */}
+          <Route path="/SignInPage"   element ={<SignInPage/>} />
           <Route path="/admin"   element ={<AdminMainPage/>} />
           <Route path="/admin/userManagement"   element ={<UserManagement/>} />
           <Route path="/admin/bookManagement"   element ={<BookManagement/>} />
+
+          <Route path="/AddBook"   element ={<BookAdd/>} />
+          <Route path="/RemoveBook"   element ={<BookRemove/>} />
+          <Route path="/UpdateBook"   element ={<BookUpdate/>} />
+
+
         </Routes>
       </div>
+      <MainFooter/>
     </div>
+
     )
   }
 }
--- front-end/src/Components/NavbarComponent.jsx
+++ front-end/src/Components/NavbarComponent.jsx
@@ -8,29 +8,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import SignInPage from "../Containers/SignInPage/SignInPage";
 
 const NavbarComponent = ()=> {
 
   const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(true);
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-  
-  const [showSignUpPopup, setShowSignUpPopup] = useState(false); // Control the visibility of the signup popup
-
-  const openSignUpPopup = () => {
-    setShowSignUpPopup(true);
-  };
-
-  const closeSignUpPopup = () => {
-    setShowSignUpPopup(false);
-  };
-
-  const changeIcon = () => {
-    setIsUserLoggedIn(true);
-  }
-
+   
     return (
-       <div>
+       
       <Navbar bg="dark" variant="dark" expand="lg" className="navbar" w>
       <Container className='container' >
         <Navbar.Brand as={Link} className="nav-item na-link" to="/">LBMS</Navbar.Brand>
@@ -58,25 +42,19 @@ const NavbarComponent = ()=> {
           
           <Nav.Link as={Link} className="nav-item nav-link" to="/ContactUs" style={{color: "white"}}>Contact Us</Nav.Link>
           {isAdminLoggedIn && <Nav.Link as={Link} className="nav-item nav-link" to="/admin" style={{color: "white"}}>Admin</Nav.Link>}
-          <button
-            className="login-button-css"
-            onClick={openSignUpPopup} // Open the signup popup when clicked
-          >
-            Sign in
-          </button>
-          <button className="login-button-css padding-css" onClick={openSignUpPopup}>
-            Sign up
-          </button>
+
+          <Link to="/SignInPage">
+              <button className="login-button-css" >Sign in</button>
+            </Link>
+              <button className="login-button-css padding-css" >sign up</button>
+
             
         </Navbar.Collapse>
       </Container>
-      {showSignUpPopup && (
-        <SignInPage onClose={closeSignUpPopup} onSuucessClose={changeIcon} />
-      )}
+
 
     </Navbar>
- 
-    </div>
+
 
 
   )
