d420bca67d308a8f9661a844a4b25105fd5903c9
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-09-06 02:33:14 +0530

    Update header.jsx

--- front-end/src/Components/Header/Header.jsx
+++ front-end/src/Components/Header/Header.jsx
@@ -8,105 +8,89 @@ import SignInPage from "../../Containers/SignInPage/SignInPage";
 import auth from "../../services/authService";
 
 const Header = () => {
-          const [showSignUpPopup, setShowSignUpPopup] = useState(false);
-          const [isUserLogIn, setUserLogIn] = useState(false);
-          const [isAdminLoggedIn, setAdminLoggedIn] = useState(false);
-          const [menuOpen, setMenuOpen] = useState(false);
+  const [showSignUpPopup, setShowSignUpPopup] = useState(false);
+  const [isUserLogIn, setUserLogIn] = useState(false);
+  const [isAdminLoggedIn, setAdminLoggedIn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-          const openSignUpPopup = () => {
-            setShowSignUpPopup(true);
-          };
+  const openSignUpPopup = () => {
+    setShowSignUpPopup(true);
+  };
 
-          const closeSignUpPopup = () => {
-            setShowSignUpPopup(false);
-          };
-          const changeIconLogIn = () => {
-            setUserLogIn(true);
-          };
+  const closeSignUpPopup = () => {
+    setShowSignUpPopup(false);
+  };
 
-          const changeIconLogOut = () => {
-            auth.logout(); // Call the logout function from your authService
-            setUserLogIn(false); // Set isUserLogIn to false when logged out
-            setAdminLoggedIn(false)
-          };
+  const changeIconLogIn = () => {
+    setUserLogIn(true);
+  };
 
+  const changeIconLogOut = () => {
+    auth.logout(); // Call the logout function from your authService
+    setUserLogIn(false); // Set isUserLogIn to false when logged out
+    setAdminLoggedIn(false);
+  };
 
+  const getMenuStyles = (menuOpen) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpen && "-100%" };
+    }
+  };
 
-          const getMenuStyles = (menuOpen) => {
-            if (document.documentElement.clientWidth <= 800) {
-              return { right: !menuOpen && "-100%" };
-            }
-          };
+  // Use useEffect to check the user's login status on component mount
+  useEffect(() => {
+    const user = auth.getCurrentUser();
+    
+    // Assuming user.isAdmin is correctly set when a user is an admin
+    if (user && user.isAdmin) {
+      setUserLogIn(true);
+      setAdminLoggedIn(true);
+    } else if (user) {
+      setUserLogIn(true);
+      setAdminLoggedIn(false);
+    } else {
+      setUserLogIn(false);
+      setAdminLoggedIn(false);
+    }
+  }, []);
 
-          // Use useEffect to check the user's login status on component mount
-          useEffect(() => {
-            const user = auth.getCurrentUser();
-            try {
-              if (user.name) {
-                setUserLogIn(true);
-        
-                if (user.isAdmin) {
-                  setAdminLoggedIn(true);
-                } else {
-                  setAdminLoggedIn(false); // Ensure isAdminLoggedIn is false when not admin
-                }
-              } else {
-                setUserLogIn(false);
-                setAdminLoggedIn(false); // Ensure isAdminLoggedIn is false when not logged in
-              }
-            } catch { 
-              setUserLogIn(false);
-              setAdminLoggedIn(false); // Ensure isAdminLoggedIn is false on error
-            }
-          }, []);
-          
-         
-          return (
-              
-            <section className="h-wrapper ">
-              <div
-                className="flexCenter padding innerWidth h-container"
-                id="h-container"
-              >
-                <a href="">
-                  <img src={logo} alt="logo" className="img" height={80} />
-                </a>
-                <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
+  return (
+    <section className="h-wrapper">
+      <div className="flexCenter padding innerWidth h-container" id="h-container">
+        <a href="">
+          <img src={logo} alt="logo" className="img" height={80} />
+        </a>
+        <OutsideClickHandler onOutsideClick={() => setMenuOpen(false)}>
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
+            <Link to="/" className="navbar-elements">
+              Home
+            </Link>
+            <Link to="/Library" className="navbar-elements">
+              Library
+            </Link>
+            <Link className="navbar-elements">About Us</Link>
+            <Link to="/ContactUs" className="navbar-elements">
+              Contact Us
+            </Link>
+            {isAdminLoggedIn && <Link className="nav-item nav-link" to="/admin">Admin</Link>}
+            <Link>
+              {isUserLogIn ? (
+                <button className="button" onClick={changeIconLogOut}>Log Out</button>
+              ) : (
+                <button className="button" onClick={openSignUpPopup}>Sign Up</button>
+              )}
+            </Link>
+          </div>
+        </OutsideClickHandler>
+        <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
+          <BiMenuAltRight size={30} />
+        </div>
+        {showSignUpPopup && (
+          <SignInPage onClose={closeSignUpPopup} onSuucessClose={changeIconLogIn} />
+        )}
+      </div>
+    </section>
+  );
+};
 
-                  <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
-                    <Link to="/" className="navbar-elements">
-                      Home
-                    </Link>
-                    <Link to="/Library" className="navbar-elements">
-                      Library
-                    </Link>
-                    <Link className="navbar-elements">About Us</Link>
-                    <Link to="/ContactUs" className="navbar-elements">
-                      Contact Us
-                    </Link>
-                    {isAdminLoggedIn && <Link className="nav-item nav-link" to="/admin">Admin</Link>}
-
-                    <Link >
-
-                      {isUserLogIn ?
-                        <button className="button" onClick={changeIconLogOut}> Log Out</button> :
-                        <button className="button" onClick={openSignUpPopup}> Sign Up</button>
-                      }
-
-                    </Link>
-                  </div>
-                </OutsideClickHandler>
-                <div className="menu-icon" onClick={() => { setMenuOpen((prev) => !(prev)) }}>
-                  <BiMenuAltRight size={30} />
-                </div>
-                {showSignUpPopup && (
-                  <SignInPage onClose={closeSignUpPopup} onSuucessClose={changeIconLogIn} />
-                )}
-              </div>
-
-            </section>
-
-          );
-        };
-
-        export default Header;
+export default Header;
