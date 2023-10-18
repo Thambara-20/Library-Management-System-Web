5580fadd7823a89d1a12453cc7ec25ee820858dc
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-10-18 23:32:07 +0530

    completed notification service

--- front-end/src/Components/NotificationCard.js
+++ front-end/src/Components/NotificationCard.js
@@ -15,39 +15,71 @@ const NotificationCard = ({ title, book_id, return_date, is_returned, email, is_
     }
     return (
 
-        <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '2px', marginTop: '2px', width: '100%', backgroundColor: 'grey', padding: '10px 10px 10px 10px', borderRadius: '5px' }}>
-            <div className='main-notification'style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%' ,transition: 'transform 0.2s'}}>
-             <style>
-                {`
-                   main-notification :hover {
-                    transform: scale(1.1); 
-                    }
-                `}
-            </style>
+        <div className="wrapper-main" style={{ display: 'flex', flexDirection: 'column', marginBottom: '2px', marginTop: '2px', width: '100%', padding: '10px 10px 10px 10px', borderRadius: '5px'
+        ,color: 'darkred !important',  
+         background: 'black !important', 
+         boxShadow: '0 10px 10px rgba(0, 0, 0, 0.7)',
+         transition: 'transform 0.2s, color 0.2s',
+
+         color: 'darkred !important', }}>
+            <div className='main-notification'style={{ 
+                display: 'flex', flexDirection: 'row', 
+                justifyContent: 'space-between', alignItems: 'center',
+                 width: '100%' ,transition: 'transform 0.2s',
+                 color: 'darkred !important',   backgroundColor: 'black !important',
+                 }}>
                 <div style={{ display: 'flex', flex: 0.5 }}>
                     <WarningIcon style={{ color: 'rgb(181, 0, 0)', fontSize: 120 ,  boxShadow: '0 10px 10px rgba(0, 0, 0, 0.5)',borderRadius:'50%', padding:'10px 10px 10px 10px', }} />
                 </div>
                 <div style={{ display: 'flex', flex: 4, width: '100%', justifyContent: 'space-between' }}>
-                    <div style={{ display: 'flex', flexDirection: 'column', padding: '5px 5px 5px 5px' }}>
-
-                        <p style={{ fontSize: 18 ,fontWeight:500 }}> You have not returned the book <strong>{title}</strong> id-{book_id} </p>
-                        <p style={{ fontSize: 16, fontWeight:500 }}> Return date <strong>{return_date}</strong> expired. Please return the book as soon as possible </p>
-                        <p style={{ fontSize: 16 ,fontWeight:500 }}> See your email {email} for more information, you will be charged per day</p>
+                    <div style={{ display: 'flex', flexDirection: 'column', padding: '5px 5px 5px 5px',
+                     color: 'darkred !important',
+ }}>
+                        <p style={{ fontSize: 18 ,fontWeight:400,color: 'white', }}> You have not returned the book <strong>{title}</strong> id-{book_id} </p>
+                        <p style={{ fontSize: 16, fontWeight:400 ,color: 'white',}}> Return date <strong>{return_date}</strong> expired. Please return the book as soon as possible </p>
+                        <p style={{ fontSize: 16 ,fontWeight:400,color: 'white', }}> See your email {email} for more information, you will be charged</p>
 
 
                     </div>
                     {!read && (
-                        <button onClick={markAsRead} style={{ fontSize: 16, background: 'transparent', padding: '5px', border: 'none', cursor: 'pointer', borderRadius: '5px', minWidth: '100px', transition: 'transform 0.2s, color 0.2s' }}>
-                            <DeleteForeverIcon style={{ fontSize: 55 }} />
-                            <style>
-                                {`
-                                    button:hover {
-                                    transform: scale(1.1); 
-                                    background-color: green;
-                                    }
-                                `}
-                            </style>
-                        </button>
+                        <button
+                        onClick={markAsRead}
+                        style={{
+                          fontSize: 16,
+                          background: 'transparent',
+                          padding: '5px',
+                          border: 'none',
+                          cursor: 'pointer',
+                          borderRadius: '5px',
+                          minWidth: '100px',
+                          transition: 'transform 0.2s, color 0.2s',
+                        }}
+                      >
+                        <DeleteForeverIcon className="icondelete" style={{ fontSize: 55, color: 'grey',borderRadius:'10px' }} />
+                        <style>
+                          {`
+                            button:hover {
+                              color: red !important;
+                              transform: scale(1.1); 
+                            }
+                            .icondelete:hover {
+                              color: darkred !important;
+                              background-color: black !important;
+                              box-shadow: 0 10px 10px rgba(0, 0, 0, 0.7);
+                            }
+                            .wrapper-main:hover {
+                                scale: 1.1;
+                                color: darkred !important;
+                                background-color: black !important;
+                                box-shadow: 0 10px 10px rgba(0, 0, 0, 0.7);
+                              }
+                            .p:hover{
+                                color: darkred !important;
+                            }
+                         
+                          `}
+                        </style>
+                      </button>
                     )}
 
                 </div>
--- front-end/src/Containers/Notifications/Notifications.jsx
+++ front-end/src/Containers/Notifications/Notifications.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
 import { userReservedDummy as userReserved } from "../../Helpers/UserReservedDummy";
 import { NotificationCard } from '../../Components/NotificationCard';
+import { notificationService } from '../../services/reservationService';
+import LoadingIcon from '../../Components/LoadingIcon';
+
 
 const Notifications = () => {
+const [notifications, setNotifications]= useState();
+
+useEffect(() => {
+  async function fetchData() {
+    try {
+      const response = await notificationService();
+      setNotifications(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  fetchData();
+},
+ []);
+
+ if (notifications===undefined) {
+   
+   return <LoadingIcon/>}
   
   return (
     <div data-aos='fade-up'>
     
       <div style={{ justifyContent:'center', display:'flex', flexDirection:'column', alignItems:'center'}}>
-          {userReserved.map((book) => (
+          {notifications.map((book) => (
             <NotificationCard key ={book.id} title = {book.title} book_id={2}/>
           ))}
         </div>
