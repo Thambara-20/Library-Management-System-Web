2ae850d9fef64656d43e9c6e19a2391a97ff0dba
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-10-27 18:07:23 +0530

    updated routes

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App";
 import reportWebVitals from "./ReportWebVitals";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter} from "react-router-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
@@ -10,9 +10,9 @@ import "font-awesome/css/font-awesome.css";
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 reportWebVitals();
