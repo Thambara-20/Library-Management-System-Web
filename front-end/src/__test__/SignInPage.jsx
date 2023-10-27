6ed1c14251606f77e990be46035ef2ec3dd7b73c
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-10-27 19:22:22 +0530

    Revert "Remove files"
    This reverts commit b9f7acaf57a006b2ee58bcc272a2702e91a215b1.

---
+++ front-end/src/__test__/SignInPage.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import './SignIn.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../services/authService';
+import "font-awesome/css/font-awesome.css";
+
+
+const SignInPage = ({ onClose, onSuucessClose }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    password: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await login(formData);
+      onClose();
+      onSuucessClose();
+      navigate("/");
+
+    } catch (error) {
+      console.error("Error during login:", error);
+      setErrorMessage("An error occurred during login.");
+    }
+  };
+
+  return (
+    <div className="popup-wrapper">
+      <div className="popup">
+        <button className="popup-close-button" onClick={onClose}>
+          Close
+        </button>
+        <div className="frame">
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group" >
+
+
+                <div className="text-wrapper">Login</div>
+              <div className='login-wrapper'>
+
+                <form onSubmit={handleSubmit}>
+                  <div className="input-container">
+                    <label className="input-label">User name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleInputChange}
+                      className="input-field"
+                      required
+                    />
+                  </div>
+                  <div className="input-container">
+                    <label className="input-label">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleInputChange}
+                      className="input-field"
+                      required
+                    />
+                  </div>
+                  <div className="error-message">{errorMessage}</div>
+
+                  <div className="checkbox-container">
+                    <input type="checkbox" id="rememberMe" />
+                    <label htmlFor="rememberMe">Remember Me</label>
+                  </div>
+                  <div className="wrapper">
+                    <div className="reg-log-button">
+                      <div className="reg-button">
+                        <button className="btn btn-primary" type="submit">Login</button>
+                      </div>
+                    </div>
+                    <div className="reg-log-button">
+                      <Link to={'/Register'} id='link'>
+                        <div className="reg-button">
+                          <button className="btn btn-primary">Register</button>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+
+                <div className="divider"></div> {/* Add this line to create the black line */}
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
+
+
+
---
+++ front-end/src/__test__/SignUp.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import RegForm from "../Components/RegForm/RegForm";
+import "./SignUp.css";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button } from "@mui/material";
+
+
+const SignUp = () => {
+  return (
+    <div>
+
+      <Link to="/" style={{ top: "10px", left: "10px", position: "fixed", zIndex: "100" }}>
+        <Button
+          variant="contained"
+          startIcon={<ArrowBackIcon style={{ color: 'grey' }} />}
+          style={{ backgroundColor: 'black', color: 'white', }}>
+          Back
+        </Button>
+      </Link>
+      <section className="register-wrapper">
+        <div className="paddings innerWidth flexCenter reg-container">
+          <div className="right">
+            <form action="" className="reg-form">
+              <span className="primaryText">Register</span>
+              <div className="name">
+                <RegForm Label="First Name" placeHolder="First Name" />
+                <RegForm Label="Last Name" placeHolder="Last Name" />
+              </div>
+              <RegForm
+                Label="Address"
+                placeHolder="Address"
+              />
+              <RegForm
+                Label="Email"
+                type="email"
+                placeHolder="email"
+              />
+              <RegForm Label="ID number" placeHolder="id" />
+
+              <div className="name">
+                <RegForm Label="Password" type="password" placeHolder="* * * * * * *" />
+                <RegForm Label="Confirm Password" type="password" placeHolder="* * * * * * *" />
+              </div>
+
+              <div className="reg-log-button">
+                <div className="reg-button">
+                  <button className="btn btn-primary" type="submit">Sign up</button>
+                </div>
+
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignUp;
