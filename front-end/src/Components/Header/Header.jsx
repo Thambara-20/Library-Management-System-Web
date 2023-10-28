9832454f26de1d47ad1dca01186fbd5bab811a21
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-10-28 21:58:01 +0530

    updated header

--- front-end/src/Components/Header/Header.jsx
+++ front-end/src/Components/Header/Header.jsx
@@ -3,12 +3,11 @@ import logo from "./logo.png";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import { BiMenuAltRight } from "react-icons/bi";
-import OutsideClickHandler from 'react-outside-click-handler';
-
-import { AiOutlineHome } from 'react-icons/ai';
-import { HiOutlineBuildingLibrary } from 'react-icons/hi2';
-import { GoPeople } from 'react-icons/go'
-import { ImMail2 } from 'react-icons/im'
+import OutsideClickHandler from "react-outside-click-handler";
+import { AiOutlineHome } from "react-icons/ai";
+import { HiOutlineBuildingLibrary } from "react-icons/hi2";
+import { GoPeople } from "react-icons/go";
+import { ImMail2 } from "react-icons/im";
 import SignInPage from "../../Containers/SignInPage/SignInPage";
 import auth from "../../services/authService";
 import HeaderDropDown from "./HeaderDropDown";
@@ -17,7 +16,7 @@ const Header = () => {
   const [showSignUpPopup, setShowSignUpPopup] = useState(false);
   const [isUserLogIn, setUserLogIn] = useState(true);
   const [isAdminLoggedIn, setAdminLoggedIn] = useState(true);
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const openSignUpPopup = () => {
     setShowSignUpPopup(true);
@@ -32,31 +31,28 @@ const Header = () => {
   };
 
   const changeIconLogOut = () => {
-
     auth.logout();
     setUserLogIn(false);
     setAdminLoggedIn(false);
   };
 
-
   const getMenuStyles = (menuOpen) => {
-    // if (document.documentElement.clientWidth <= 800) {
-    //   return { right: !menuOpen && "-100%" };
-    // }
     if (document.documentElement.clientWidth <= 800) {
-    return { visibility: menuOpen ? "visible" : "hidden" };
-  }
+      return { visibility: menuOpen ? "visible" : "hidden" };
+    }
   };
 
-
-
-
-  const handleTogleButton = (event) => {
+  const handleTogleButton = () => {
     if (menuOpen) {
-      const temp = !menuOpen;
-      setMenuOpen(temp);
-      console.log(temp);
+      setMenuOpen(false);
+    } else {
+      setMenuOpen(true);
     }
+  };
+
+  const handleMenuItemClick = () => {
+    // Close the menu when a menu item is clicked
+    setMenuOpen(false);
   };
 
   useEffect(() => {
@@ -74,7 +70,6 @@ const Header = () => {
     }
   }, []);
 
-  
   return (
     <section className="h-wrapper">
       <div className="flexCenter padding innerWidth h-container" id="h-container">
@@ -83,45 +78,70 @@ const Header = () => {
         </Link>
         <OutsideClickHandler onOutsideClick={handleTogleButton}>
           <div className="flexCenter h-menu" style={getMenuStyles(menuOpen)}>
-            <Link to="/" className="navbar-elements" style={{ textDecoration: "none" }}>
-              <AiOutlineHome style={{paddingBottom:4,paddingRight:2}} size={20}/>Home
+            <Link
+              to="/"
+              className="navbar-elements"
+              style={{ textDecoration: "none" }}
+              onClick={handleMenuItemClick}
+            >
+              <AiOutlineHome style={{ paddingBottom: 4, paddingRight: 2 }} size={20} />
+              Home
             </Link>
-            <Link to="/Library" className="navbar-elements" style={{ textDecoration: "none" }}>
-              <HiOutlineBuildingLibrary style={{paddingBottom:4,paddingRight:2}} size={20}/>Library
+            <Link
+              to="/Library"
+              className="navbar-elements"
+              style={{ textDecoration: "none" }}
+              onClick={handleMenuItemClick}
+            >
+              <HiOutlineBuildingLibrary style={{ paddingBottom: 4, paddingRight: 2 }} size={20} />
+              Library
             </Link>
-            <Link to= "/AboutUs" className="navbar-elements" style={{ textDecoration: "none" }}>
-              <GoPeople style={{paddingBottom:4,paddingRight:2}} size={20}/>About Us
+            <Link
+              to="/AboutUs"
+              className="navbar-elements"
+              style={{ textDecoration: "none" }}
+              onClick={handleMenuItemClick}
+            >
+              <GoPeople style={{ paddingBottom: 4, paddingRight: 2 }} size={20} />
+              About Us
             </Link>
-            <Link to="/ContactUs" className="navbar-elements" style={{ textDecoration: "none" }}>
-              <ImMail2 style={{paddingBottom:4,paddingRight:2}} size={20}/>Contact Us
+            <Link
+              to="/ContactUs"
+              className="navbar-elements"
+              style={{ textDecoration: "none" }}
+              onClick={handleMenuItemClick}
+            >
+              <ImMail2 style={{ paddingBottom: 4, paddingRight: 2 }} size={20} />
+              Contact Us
             </Link>
             {isAdminLoggedIn && (
-              <Link className="navbar-elements" to="/admin" style={{ textDecoration: "none" }}>
+              <Link
+                className="navbar-elements"
+                to="/admin"
+                style={{ textDecoration: "none" }}
+                onClick={handleMenuItemClick}
+              >
                 Admin
               </Link>
             )}
             <Link>
-              {isUserLogIn ?
-              (<HeaderDropDown onLogout={changeIconLogOut}/> ) : 
-              (<button className="button" onClick={openSignUpPopup}>Login</button>
+              {isUserLogIn ? (
+                <HeaderDropDown onLogout={changeIconLogOut} />
+              ) : (
+                <button className="button" onClick={openSignUpPopup}>
+                  Login
+                </button>
               )}
             </Link>
-
           </div>
-        <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
-
-          <BiMenuAltRight size={30} />
-        </div>
+          <div className="menu-icon" onClick={() => setMenuOpen((prev) => !prev)}>
+            <BiMenuAltRight size={30} />
+          </div>
         </OutsideClickHandler>
-       
-        {showSignUpPopup && (
-          <SignInPage onClose={closeSignUpPopup} onSuucessClose={changeIconLogIn} />
-        )}
+        {showSignUpPopup && <SignInPage onClose={closeSignUpPopup} onSuucessClose={changeIconLogIn} />}
       </div>
     </section>
   );
 };
 
 export default Header;
-
-
